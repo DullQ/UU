3a7ec1c368ff579ca989c5acd1b0fa167d48bc35
DullQ <84604962+DullQ@users.noreply.github.com>
2022-01-20 18:43:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload(){ 
+}
+
+function setup() {
+    canvas = createCanvas(700, 600);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(700, 600);
+    video.hide();
+
+    poseNet = ml5.poseNET(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y)
+    }    
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 700, 600);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
